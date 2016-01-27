a685e0e9bd8199e1be5c79513185059b3539b6ca
tureki <tureki@me.com>
2016-01-27 18:43:55 +0800

    add React Render Server JS config files

---
+++ src/config/rrs/src/HelloWorld.jsx
@@ -0,0 +1,15 @@
+'use strict';
+
+import React from 'react';
+
+export class HelloWorld extends React.Component {
+  render() {
+    return (
+      <div>
+        React Render Server -> Hello World :)
+      </div>
+    );
+  }
+};
+
+module.exports = HelloWorld;
